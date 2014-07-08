70f0bd586b616351cd2c60c76949c1973e154279
AngelRoca <angelroca891020@gmail.com>
2014-07-08 04:01:51 -0600

    actualizacion en la base de datos

---
+++ sql/factura_ordenador.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.9deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 08-07-2014 a las 04:58:02
+-- Versión del servidor: 5.5.37-1
+-- Versión de PHP: 5.5.9-1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `factura_ordenador`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id_rfc_cliente` varchar(13) NOT NULL,
+  `nombre_cliente` varchar(45) NOT NULL,
+  `domicilio` varchar(45) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_rfc_cliente`),
+  UNIQUE KEY `id_rfc:cliente_UNIQUE` (`id_rfc_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE IF NOT EXISTS `factura` (
+  `id_factura` int(11) NOT NULL AUTO_INCREMENT,
+  `id_rfc_cliente` varchar(13) NOT NULL,
+  `fecha_expedicion` date NOT NULL,
+  `lugar_expedicion` varchar(30) NOT NULL,
+  `forma_pago` varchar(20) NOT NULL,
+  `cantidad` longtext NOT NULL,
+  `descripcion` longtext NOT NULL,
+  `precio_unitario` longtext NOT NULL,
+  `importe` longtext NOT NULL,
+  `subtotal` double NOT NULL,
+  `iva` int(11) NOT NULL,
+  `total` double NOT NULL,
+  PRIMARY KEY (`id_factura`),
+  UNIQUE KEY `id_factura_UNIQUE` (`id_factura`),
+  UNIQUE KEY `id_rfc_cliente_UNIQUE` (`id_rfc_cliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
+  `producto` varchar(45) NOT NULL,
+  `existencia` int(11) NOT NULL,
+  `precio` int(11) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  UNIQUE KEY `id_producto_UNIQUE` (`id_producto`),
+  UNIQUE KEY `producto_UNIQUE` (`producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `permisos` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_usuario_UNIQUE` (`id`),
+  UNIQUE KEY `nombre_usuario_UNIQUE` (`nombre`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
